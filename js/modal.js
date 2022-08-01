d5bad17cf67e08889b2320ad2659c278d119f2e6
TryHungury <mukolanadebernuy@gmail.com>
2022-08-01 16:31:23 +0300

    Обновив форму

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,18 @@
       refs.modal.classList.toggle("is-hidden");
     }
   })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-use]"),
+      closeModalBtn: document.querySelector("[data-modal-unuse]"),
+      use: document.querySelector("[data-use]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.use.classList.toggle("is-hidden");
+    }
+  })();
